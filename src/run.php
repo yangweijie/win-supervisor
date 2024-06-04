2a9ba3a00899879099eac381506f75f48c6a0e24
yangweijie <917647288@qq.com>
2024-06-04 17:06:36 +0800

    更新dll 支持更高版php-webview

--- src/run.php
+++ src/run.php
@@ -18,15 +18,13 @@ use KingBes\PhpWebview\WebView;
 use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Output\OutputInterface;
 
-use Symfony\Component\Process\Exception\ProcessFailedException;
 use Symfony\Component\Process\Process;
 
 $app = new Silly\Application();
-global $php_bin,$think_file,$web_root,$web_port,$web_pid_file,$console_bin,$bat_file;
+global $php_bin,$think_file,$web_root,$web_port,$console_bin,$bat_file;
 $console_bin = __DIR__.'/../php/console.exe';
 $php_bin = __DIR__.'/../php/php.exe';
 $bat_file = __DIR__.'/web.bat';
-$web_pid_file = __DIR__.'/../php/web_pid';
 $think_file = __DIR__.'/think';
 $web_root = __DIR__.'/public';
 $web_port = 8080;
@@ -102,14 +100,14 @@ $app->command('supervisord-web:start', function ($input, $output) {
 
         $web->start();
         $web->wait();
-        $output->writeln('web running');
-//        file_put_contents($web_pid_file, $web->getPid());
+        $output->writeln('Web is running');
     }
 });
 
 function get_pid_from_port_str($str){
     return str_replace([' ', 'LISTENING'], ['',''], strstr($str, 'LISTENING'));
 }
+
 $app->command('supervisord-web:stop', function ($input, $output) {
     global $web_port;
     if($ret = is_web_running($web_port)){
@@ -123,24 +121,17 @@ $app->command('supervisord-web:stop', function ($input, $output) {
             $output->writeln('程序无法杀死为知pid的进程，请任务管理器里结束进程');
         }
     }else{
-        $output->writeln('web not running');
+        $output->writeln('Web not running');
     }
 });
 
-$app->command('supervisord:run-gui', function ($input, $output) use($app){
-    global $web_port,$php_bin;
+$app->command('supervisord:gui', function ($input, $output) use($app){
+    global $web_port;
     if(!is_web_running($web_port)){
-        $greetInput = new ArrayInput([
-            // the command name is passed as first argument
-            'command' => 'supervisord-web:start',
-        ]);
-        $returnCode = $app->doRun($greetInput, $output);
-        if($returnCode !== 0){
-            $output->writeln("web can't start");
-            return $returnCode;
-        }
+        $output->writeln("Web not running");
+        return 404;
     }
-    $webview = new WebView('supervisor 管理', 800, 600, true, __DIR__.'../');
+    $webview = new WebView('supervisor 管理', 800, 600, true, __DIR__);
     $url = "http://localhost:{$web_port}";
     $webview->navigate($url);
     // 运行
